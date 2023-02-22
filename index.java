1a44238744ef7909407f3a3489e7436a855b0ecb
lettopk <mtarazona97@uan.edu.co>
2023-02-22 15:20:08 -0500

    first commit

---
+++ index.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class index {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "holis", "Informacion",JOptionPane.QUESTION_MESSAGE);
+
+    }
+}
